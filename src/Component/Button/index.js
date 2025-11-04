5a5ffc70587af711c70392abf357464fe230524c
박연수 <bag-yeonsu@bag-yeonsuui-MacBookAir.local>
2025-11-04 17:24:46 +0900

    Build : build error edit

--- src/Component/Button/index.js
+++ src/Component/Button/index.js
@@ -1,52 +1,52 @@
-import React from "react";
-import styled, {css} from "styled-components"
-import Color from "../../Styles/color"
-import { fontWeight, fontSize } from "../../Styles/fonts"
+// import React from "react";
+// import styled, {css} from "styled-components"
+// import Color from "../../Styles/color"
+// import { fontWeight, fontSize } from "../../Styles/fonts"
 
-const Btn = styled.button`
-    display: flex;
-    height: 33px;
-    width: 100%;
-    border: none;
-    color : #ffffff;
-    font-weight : ${fontWeight.bold};
-    font-size : ${fontSize.small};
-    justify-content: center;
-    align-items: center;
-    margin-top: 1rem;
-    border-radius: 5px;
-    cursor : pointer;
-    ${props =>
-        props.variant === 'submitBtn' &&
-        css`
-            background: ${Color.btn};
-        `
-    }
-    ${props =>
-        props.variant === 'buttonBtn' &&
-        css`
-            background: none;
-            color : #000000;
-            font-weight : ${fontWeight.normal};
-            margin-top : 0.5rem;
-        `
-    }
-    & :hover{
-        background : ${Color.hoverBtn}
-    }
-    & :disabled {
-        background : ${Color.btn}
-    }
-`
+// const Btn = styled.button`
+//     display: flex;
+//     height: 33px;
+//     width: 100%;
+//     border: none;
+//     color : #ffffff;
+//     font-weight : ${fontWeight.bold};
+//     font-size : ${fontSize.small};
+//     justify-content: center;
+//     align-items: center;
+//     margin-top: 1rem;
+//     border-radius: 5px;
+//     cursor : pointer;
+//     ${props =>
+//         props.variant === 'submitBtn' &&
+//         css`
+//             background: ${Color.btn};
+//         `
+//     }
+//     ${props =>
+//         props.variant === 'buttonBtn' &&
+//         css`
+//             background: none;
+//             color : #000000;
+//             font-weight : ${fontWeight.normal};
+//             margin-top : 0.5rem;
+//         `
+//     }
+//     & :hover{
+//         background : ${Color.hoverBtn}
+//     }
+//     & :disabled {
+//         background : ${Color.btn}
+//     }
+// `
 
-const Button = ({onClick, children, type = "button",variant, ...props}) => {
-    return (
-        <>
-            <Btn type={type} onClick={onClick} variant={variant} {...props}>
-                {children}
-            </Btn>
-        </>
-    )
-}
+// const Button = ({onClick, children, type = "button",variant, ...props}) => {
+//     return (
+//         <>
+//             <Btn type={type} onClick={onClick} variant={variant} {...props}>
+//                 {children}
+//             </Btn>
+//         </>
+//     )
+// }
 
-export default Button;
+// export default Button;
--- src/auth/authProvider.js
+++ src/auth/authProvider.js
@@ -6,60 +6,68 @@ import { useNavigate } from "react-router-dom";
 const AuthContext = createContext();
 
 export const useAuth = () => {
-    return useContext(AuthContext);
+  return useContext(AuthContext);
 };
 
-export const AuthProvider = ({children}) => {
-    const navigate = useNavigate();
-    const [cookies, setCookie, removeCookie] = useCookies(['userData']);
-    const [isLogin, setIsLogin] = useState(false);
-    const [user, setUser] = useState(null);
-    
-    const login = (userData) => {
-        console.log("userData : ", userData);
-        setIsLogin(true);
-        setUser(userData);
-        setCookie('userData', JSON.stringify(userData), { 
-            path:'/',
-            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000) 
+export const AuthProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [, setCookie, removeCookie] = useCookies(["userData"]);
+  const [isLogin, setIsLogin] = useState(false);
+  const [user, setUser] = useState(null);
+
+  const login = (userData) => {
+    console.log("userData : ", userData);
+    setIsLogin(true);
+    setUser(userData);
+    setCookie("userData", JSON.stringify(userData), {
+      path: "/",
+      expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+    });
+  };
+
+  const logoutHandler = async () => {
+    try {
+      const res = await axios.get(
+        "http://localhost:4000/user/logout",
+        {},
+        { withCredentials: true }
+      );
+      if (res.status === 200) {
+        setIsLogin(false);
+        setUser(null);
+        removeCookie("userData", { path: "/" });
+        navigate("/");
+      }
+    } catch (error) {
+      console.log("logout failed : ", error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await axios.get("http://localhost:4000/user", {
+          withCredentials: true,
         });
-    };
-
-    const logoutHandler = async () => {
-        try {
-            const res = await axios.get('http://localhost:4000/user/logout', {}, { withCredentials : true});
-            if(res.status === 200){
-                setIsLogin(false);
-                setUser(null);
-                removeCookie('userData', {path: '/'});
-                navigate('/');
-            }
-        } catch(error){
-            console.log("logout failed : ", error);
+        if (res.status === 200) {
+          setUser(res.data.user);
+          setIsLogin(true);
+        } else {
+          setIsLogin(false);
         }
+      } catch (error) {
+        console.error("Session check failed :", error);
+        setIsLogin(false);
+      }
     };
+    fetchUser();
+  }, []);
 
-    useEffect(() => {
-        const fetchUser = async() => {
-                try {
-                    const res = await axios.get('http://localhost:4000/user', { withCredentials : true});
-                    if (res.status === 200) {
-                        setUser(res.data.user);
-                        setIsLogin(true);
-                    } else {
-                        setIsLogin(false);
-                    }
-                } catch (error) {
-                    console.error('Session check failed :', error);
-                    setIsLogin(false);
-                }
-            };
-            fetchUser();
-    },[]);
-
-    return (
-        <AuthContext.Provider value={{isLogin, setIsLogin, user, setUser, login,logoutHandler}}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider
+      value={{ isLogin, setIsLogin, user, setUser, login, logoutHandler }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
